6a6c1a5ba5d9a91bcf793e01738b06378c0449c0
Mariusz <superhiro2a@gmail.com>
2018-04-29 21:13:04 +0200

    folder na plansze

---
+++ plansza8x8/Plansza.cpp
@@ -0,0 +1,14 @@
+/*
+ * Plansza.cpp
+ *
+ *  Created on: 29.04.2018
+ *      Author: Mariusz
+ */
+
+#include "Plansza.h"
+
+Plansza::Plansza() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ plansza8x8/Plansza.h
@@ -0,0 +1,16 @@
+/*
+ * Plansza.h
+ *
+ *  Created on: 29.04.2018
+ *      Author: Mariusz
+ */
+
+#ifndef PLANSZA8X8_PLANSZA_H_
+#define PLANSZA8X8_PLANSZA_H_
+
+class Plansza {
+public:
+	Plansza();
+};
+
+#endif /* PLANSZA8X8_PLANSZA_H_ */
