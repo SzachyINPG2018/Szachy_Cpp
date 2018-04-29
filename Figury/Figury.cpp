8354b31d16e7d7d9f78cbf9ec4994953bff05d52
Mariusz <superhiro2a@gmail.com>
2018-04-29 21:08:43 +0200

    ustawione ruchy

--- Figury/Figury.cpp
+++ Figury/Figury.cpp
@@ -10,16 +10,27 @@
 Figury::Figury(int x, int y, int team, int type) : _x(x), _y(y), _team(team), _type(type)
 {
 	//--podstawowe ruchy
-//	if(type == 1)
-//	if(type == 2)
-//	if(type == 3)
-//	if(type == 4)
-//	if(type == 5)
-//	if(type == 6)
+	if(type == 1)	_possible_move = 0b11111111;
+	if(type == 2)	_possible_move = 0b11111111;
+	if(type == 3)	_possible_move = 0b1010101;
+	if(type == 4)	_possible_move = 0b10101010;
+	if(type == 5)	_possible_move = 0b111111111;
+	if(type == 6)	_possible_move = 0b00000001;
 
-//
 }
 /*
+ *
+ * 100000000 	Skoczek
+ *
+ * 10000000		NW
+ * 1000000		W
+ * 100000		SW
+ * 10000		S
+ * 1000			SE
+ * 100			E
+ * 10 			NE
+ * 1 			N
+ *
  * 1 Krol
  * 2 Hetman
  * 3 Wieza
