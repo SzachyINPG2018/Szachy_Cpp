521e523d9342eb00b8269360c228555c51327e38
Mariusz <superhiro2a@gmail.com>
2018-04-29 20:11:24 +0200

    klasy figur

--- Figury/Figury.cpp
+++ Figury/Figury.cpp
@@ -9,8 +9,33 @@
 
 Figury::Figury(int x, int y, int team, int type) : _x(x), _y(y), _team(team), _type(type)
 {
-	if(type == 1) ;
+	//--podstawowe ruchy
+//	if(type == 1)
+//	if(type == 2)
+//	if(type == 3)
+//	if(type == 4)
+//	if(type == 5)
+//	if(type == 6)
 
-	//
+//
 }
+/*
+ * 1 Krol
+ * 2 Hetman
+ * 3 Wieza
+ * 4 Goniec
+ * 5 Skoczek
+ * 6 Pion
+ *
+ * \ | /
+ * - F -
+ * / | \
+ *
+ *  x|x
+ * x | x
+ * |-S-|
+ * x | x
+ *  x|x
+ *
+ */
 
--- Figury/Figury.h
+++ Figury/Figury.h
@@ -8,21 +8,66 @@
 #ifndef FIGURY_H_
 #define FIGURY_H_
 
-class Figury {
+class Figury
+{
 public:
-	Figury(int , int , int , int );
-
-	//
+	Figury(int x, int y, int team, int type);
 
 	void set_xy(int x, int y) { _x = x; _y = y;}
 	int get_team(void)	{return _team;}
 	int get_x(void) {return _x;}
 	int get_y(void) {return _y;}
+	//int get_possible_move(void) {return _possible_move;}
+	//void set_possible_move();
+
 private:
 	int _x, _y;	//wspolrzedne
 	int _team;	//biale = 0 czy czarne = 1
 	int _type;
-	int _possible_move;
+	//int _possible_move;
+};
+/*
+* 1 Krol
+* 2 Hetman
+* 3 Wieza
+* 4 Goniec
+* 5 Skoczek
+* 6 Pion
+*/
+class Krol : public Figury
+{
+public:
+	Krol(int x, int y, int team) : Figury(x, y, team, 1) {}
+};
+
+class Hetman : public Figury
+{
+public:
+	Hetman(int x, int y, int team) : Figury(x, y, team, 2) {}
+};
+
+class Wieza : public Figury
+{
+public:
+	Wieza(int x, int y, int team) : Figury(x, y, team, 3) {}
+};
+
+class Goniec : public Figury
+{
+public:
+	Goniec(int x, int y, int team) : Figury(x, y, team, 4) {}
+};
+
+class Skoczek : public Figury
+{
+public:
+	Skoczek(int x, int y, int team) : Figury(x, y, team, 5) {}
+};
+
+class Pionek : public Figury
+{
+public:
+	Pionek(int x, int y, int team) : Figury(x, y, team, 6) {}
 };
 
 #endif /* FIGURY_H_ */
