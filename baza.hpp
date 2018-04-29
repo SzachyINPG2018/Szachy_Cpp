b939d57271a9ad29d50a6c60e50707794e0bb131
Mariusz <superhiro2a@gmail.com>
2018-04-29 16:48:31 +0200

    baza projektu

---
+++ baza.hpp
@@ -0,0 +1,5 @@
+#ifndef INCLUDE_BAZA_HPP_
+#define INCLUDE_BAZA_HPP_
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "baza.hpp"
+#include <cstdlib>
+#include <iostream>
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
